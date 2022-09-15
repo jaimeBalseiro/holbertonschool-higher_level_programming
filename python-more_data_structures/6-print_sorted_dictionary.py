82cbb992254afe705cab19847f67d2d56b8a58ca
jaimeBalseiro <jaime.diaz0303@gmail.com>
2022-09-15 16:20:23 -0400

    they didn't like my comment :(

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -4,4 +4,4 @@
 def print_sorted_dictionary(a_dictionary):
     for k, v in sorted(a_dictionary.items()):
         print("{}: {}".format(k, v))
-'''You can also use the "print(f"{}: {}") f-string that is easier and faster to read/ undestand'''
+'''You can use the print(f"{}: {}")'''
