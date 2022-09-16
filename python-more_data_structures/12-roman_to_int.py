5c533280b60bb34537f6091281a0df0ed46fc67f
jaimeBalseiro <jaime.diaz0303@gmail.com>
2022-09-16 14:18:38 -0400

    converts Roman numbers to integers

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+def roman_to_int(roman_string):
+    total = 0
+    roman_dict = {"I": 1, "V": 5, "X": 10, "L": 50,
+                  "C": 100, "D": 500, "M": 1000}
+    if isinstance(roman_string, str):
+        for i in range(len(roman_string) - 1, -1, -1):
+            integ = roman_dict[roman_string[i]]
+            if integ * 3 < total:
+                total -= integ
+            else:
+                total += integ
+        return total
+    else:
+        return 0
