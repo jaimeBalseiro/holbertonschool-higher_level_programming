f1e2f6f8fc73c21f7337de94dd15df3545b872c4
jaimeBalseiro <jaime.diaz0303@gmail.com>
2022-10-05 13:00:10 -0400

    private the instance of size in the created class

---
+++ python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Create a Square that defines a square"""
+
+class Square:
+    """
+    Class is created
+
+    Args:
+    size (int): the square represents the size of an int
+    
+    """
+    def __init__(self, size):
+        self.__size = size
