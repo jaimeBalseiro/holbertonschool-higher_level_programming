b6c84743ba0f2c4e8b99dbd6d829bcefd54af595
jaimeBalseiro <jaime.diaz0303@gmail.com>
2022-10-05 14:27:39 -0400

    little pep8 fix..........

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -10,11 +10,11 @@ class Square:
     """
     def __init__(self, size=0):
         self.__size = size
-    
+
     @property
     def size(self):
         return self.__size
-    
+
     @size.setter
     def size(self, value):
         if isinstance(value, int) is False:
@@ -23,6 +23,6 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
-        
+            
     def area(self):
         return self.__size ** 2
