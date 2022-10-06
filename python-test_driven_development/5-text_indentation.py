72c1f64d35ee8e0130e6f8471cf9c3121dc85318
jaimeBalseiro <jaime.diaz0303@gmail.com>
2022-10-06 14:20:09 -0400

    prints two new lines after chars

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""This module contins a function for testing"""
+
+
+def text_indentation(text):
+    """ Prints text and replaces certain characters with newlines
+    Args:
+    text: A string
+
+    Returns:
+    Nothing
+
+    Raises:
+    TypeError: If text is not a string
+    """
+
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    else:
+        for sep in ":.?":
+            text = (sep+"\n\n").join([row.strip() for row in text.split(sep)])
+        print(text, end="")
