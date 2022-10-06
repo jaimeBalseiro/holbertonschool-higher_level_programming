93addfd3e594f49120194916828a81592fd47e3c
jaimeBalseiro <jaime.diaz0303@gmail.com>
2022-10-06 11:38:50 -0400

    adds 2 integers

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Contains a module for tests"""
+
+
+def add_integer(a, b=98):
+    """Adds two given integers or floats
+    
+    Args:
+    a: An integer or float
+    b: An integer or float
+
+    Returns:
+    the result of a plus b
+
+    Raises:
+    TypeError: If the given parameter is not an integer or float
+    """
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+    else:
+        return int(a) + int(b)
