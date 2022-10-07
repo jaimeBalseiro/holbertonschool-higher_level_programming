1bfeef0a5d48df91db407dce8669e70903f74613
jaimeBalseiro <jaime.diaz0303@gmail.com>
2022-10-07 12:15:39 -0400

    def check

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -10,8 +10,8 @@ class Rectangle:
     height (int): Integer representing the height of a rectangle
     """
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
