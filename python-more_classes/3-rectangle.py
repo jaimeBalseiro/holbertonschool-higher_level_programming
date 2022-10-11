eacf12d222ce5c6f2ef706f2a2581b5681d7677c
jaimeBalseiro <jaime.diaz0303@gmail.com>
2022-10-11 15:10:13 -0400

    fixed some whitespaces

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -49,7 +49,7 @@ class Rectangle:
             return 0
         else:
             return 2 * (self.__width + self.__height)
-    
+
     def __str__(self):
         rectangle = []
         if self.__width == 0 or self.__height == 0:
