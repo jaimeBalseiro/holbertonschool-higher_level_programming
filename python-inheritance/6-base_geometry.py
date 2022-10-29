6c4c71e97f4f48bfcb8ac28c2a4f5099ba55f41d
jaimeBalseiro <jaime.diaz0303@gmail.com>
2022-10-29 13:19:52 -0400

    public instance method, that raises a message

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Before creating an empty class
+"""
+
+
+class BaseGeometry:
+    """
+    The creation of an empty class called BaseGeometry
+    """
+    def area(self):
+        """
+        a public instance method
+        """
+        raise Exception("area() is not implemented")
