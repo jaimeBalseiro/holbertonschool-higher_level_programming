821c559f9135fb17a7e1fcfbcc5322eb074c3ff0
jaimeBalseiro <jaime.diaz0303@gmail.com>
2022-11-06 19:14:34 -0400

    test

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,2 +1,4 @@
 -- Creates a database for usa and a table for states:
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY, name VARCHAR(256 NOT NULL));
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+  id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  name VARCHAR(256) NOT NULL);
