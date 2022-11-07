2ca3f03000b3ca358e5df22855b8787f93d52562
jaimeBalseiro <jaime.diaz0303@gmail.com>
2022-11-06 20:19:17 -0400

    teteetteetst

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,11 +1,9 @@
 -- Create a table and database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities
-(
-  id INT UNIQUE AUTO_IMCREMENT NOT NULL,
-  state_id INT NOT NULL,
-  name VARCHAR(256) NOT NULL,
-  PRIMARY KEY(id),
-  FOREIGN KEY(state_id) REFERENCES states(id)
+CREATE TABLE IF NOT EXISTS cities(
+	id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	state_id INT NOT NULL,
+	name VARCHAR(256) NOT NULL,
+	FOREIGN KEY(state_id) REFERENCES states(id)
 );
